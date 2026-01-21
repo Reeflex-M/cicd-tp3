87daed3aecc6cba8ee7bab7b17c9f91578d0d1e6
Mathis FLOCH <mathis.floch2025@campus-eni.fr>
2026-01-21 15:02:35 +0100

    first commit

---
+++ src/main/java/com/devops/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import java.util.List;
+
+public class OrderRepository {
+
+    public static List<Order> findAll() {
+        return List.of(
+            new Order(1, "Laptop", 1200.0),
+            new Order(2, "Mouse", 25.0)
+        );
+    }
+}
